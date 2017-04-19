d84b8e4473afe9760c8c3eac53bc5546b972d6c9
Chen Lu <ipaspy@gmail.com>
2017-04-19 00:05:41 -0700

    First working version

---
+++ kcsMemberCollector/MemberCollector.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace kcsMemberCollector {
+    public class MemberCollector {
+
+        public class Range { public int lo; public int hi; }
+
+        public static readonly List<string> WorldServerAddr = new List<string>() {
+               "203.104.209.71"  , // 横須賀鎮守府   
+               "203.104.209.87"  , // 呉鎮守府        
+               "125.6.184.16"    , // 佐世保鎮守府    
+               "125.6.187.205"   , // 舞鶴鎮守府      
+               "125.6.187.229"   , // 大湊警備府      
+               "125.6.187.253"   , // トラック泊地    
+               "125.6.188.25"    , // リンガ泊地      
+               "203.104.248.135" , // ラバウル基地    
+               "125.6.189.7"     , // ショートランド泊地
+               "125.6.189.39"    , // ブイン基地
+               "125.6.189.71"    , // タウイタウイ泊地 
+               "125.6.189.103"   , // パラオ泊地
+               "125.6.189.135"   , // ブルネイ泊地    
+               "125.6.189.167"   , // 単冠湾泊地      
+               "125.6.189.215"   , // 幌筵泊地        
+               "125.6.189.247"   , // 宿毛湾泊地      
+               "203.104.209.23"  , // 鹿屋基地        
+               "203.104.209.39"  , // 岩川基地        
+               "203.104.209.55"  , // 佐伯湾泊地      
+               "203.104.209.102" , // 柱島泊地        
+        };
+
+        public static int GenerateRanges(int min, int max, int numberOfRanges, out List<Range> ranges) {
+            int i;
+            int[] bucket_sizes = new int[numberOfRanges];
+            ranges = new List<Range>();
+
+            int even_length = (max - min + 1) / numberOfRanges;
+            for (i = 0; i < numberOfRanges; ++i)
+                bucket_sizes[i] = even_length;
+
+            /* distribute surplus as evenly as possible across buckets */
+            int surplus = (max - min + 1) % numberOfRanges;
+            for (i = 0; surplus > 0; --surplus, i = (i + 1) % numberOfRanges)
+                bucket_sizes[i] += 1;
+
+            int n = 0, k = min;
+            for (i = 0; i < numberOfRanges && k <= max; ++i, ++n) {
+                //ranges[i].lo = k;
+                //ranges[i].hi = k + bucket_sizes[i] - 1;
+                Range r = new Range();
+                r.lo = k;
+                r.hi = k + bucket_sizes[i] - 1;
+                ranges.Add(r);
+                k += bucket_sizes[i];
+            }
+            return n;
+        }
+
+        /// <summary>
+        /// Command line arguments
+        /// -t/--token 
+        /// -s/--server
+        /// -r/--range
+        /// -n/--numberOfThread
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args) {
+            CommandLineParser.CommandLineParser parser = new CommandLineParser.CommandLineParser();
+            CommandLineParser.Arguments.ValueArgument<string> tokenArg =
+                new CommandLineParser.Arguments.ValueArgument<string>('t', "token", "User token for Kancolle identification.");
+            tokenArg.Optional = false;
+            tokenArg.AllowMultiple = false;
+
+            CommandLineParser.Arguments.ValueArgument<string> serverArg =
+                new CommandLineParser.Arguments.ValueArgument<string>('s', "server", "Kancolle server IP address to connect.");
+            serverArg.Optional = false;
+            serverArg.AllowMultiple = false;
+
+            CommandLineParser.Arguments.ValueArgument<long> rangeArg =
+                new CommandLineParser.Arguments.ValueArgument<long>('r', "range", "Assuming member ID range.");
+            rangeArg.Optional = true;
+            rangeArg.AllowMultiple = true;
+
+            CommandLineParser.Arguments.ValueArgument<long> singleIdArg =
+                new CommandLineParser.Arguments.ValueArgument<long>('i', "id", "Single Id.");
+            singleIdArg.Optional = true;
+            singleIdArg.AllowMultiple = true;
+
+            CommandLineParser.Arguments.ValueArgument<int> numberOfThreadArg =
+                new CommandLineParser.Arguments.ValueArgument<int>('n', "thread", "Divided the task into multi threads (Max 15)");
+            numberOfThreadArg.Optional = true;
+            numberOfThreadArg.AllowMultiple = false;
+
+            parser.Arguments.Add(tokenArg);
+            parser.Arguments.Add(serverArg);
+            parser.Arguments.Add(rangeArg);
+            parser.Arguments.Add(singleIdArg);
+            parser.Arguments.Add(numberOfThreadArg);
+
+            try {
+                parser.ParseCommandLine(args);
+                int numOfThreads = 1;
+                if (numberOfThreadArg.Parsed && numberOfThreadArg.Value > 1 && numberOfThreadArg.Value <= 100) {
+                    numOfThreads = numberOfThreadArg.Value;
+                }
+
+                string token = tokenArg.Value;
+                string server = serverArg.Value;
+
+                if (singleIdArg.Parsed) {
+                    List<long> ids = singleIdArg.Values;
+                    var client = new kcClient(token, server, ids);
+                    client.StartCacheMembersAsync().Wait();
+
+                } else if (rangeArg.Parsed) {
+                    long min = rangeArg.Values[0];
+                    long max = rangeArg.Values[1];
+                    List<Range> threadGroup = null;
+                    List<Task> kcClients = new List<Task>();
+                    if (numOfThreads > 1) {
+                        int n = GenerateRanges((int)min, (int)max, numOfThreads, out threadGroup);
+
+                        for (int i = 0; i < n; i++) {
+                            kcClients.Add(new kcClient(token, server, threadGroup[i].lo, threadGroup[i].hi, "Thread ID: " + i).StartCacheMembersAsync());
+                        }
+                        Task.WhenAll(kcClients).Wait();
+
+                    } else {
+                        var client = new kcClient(token, server, min, max);
+                        client.StartCacheMembersAsync().Wait();
+
+                    }
+                }
+
+            } catch (System.Exception e) {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ kcsMemberCollector/kcClient.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace kcsMemberCollector {
+    public class kcClient {
+        private string m_clientName;
+        private string m_token;
+        private string m_server;
+        private string m_exportFileName;
+        private string m_logFileName;
+        private HttpClient m_client;
+        private List<long> m_lstMemberIds;
+        public kcClient(string token, string server, long min, long max, string clientName = "defaultClient") {
+            m_client = new HttpClient();
+            m_clientName = clientName;
+            m_token = token;
+             m_server = server;
+            m_lstMemberIds = new List<long>();
+            for (long i = min; i <= max; i++)
+                m_lstMemberIds.Add(i);
+            var rnd = new Random();
+            m_lstMemberIds = m_lstMemberIds.OrderBy(item => rnd.Next()).ToList();
+            string serverFolder = server.Replace('.', '_');
+            Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), serverFolder));
+            m_exportFileName = Path.Combine(serverFolder, string.Format("{0}-{1}_{2}.txt", m_server.Replace('.', '_'), min, max));
+            m_logFileName = Path.Combine(serverFolder, string.Format("{0}.log", m_server.Replace('.', '_')));
+            RemoveDuplicateId();
+        }
+
+        public kcClient(string token, string server, List<long> memberIds, string clientName = "defaultClient") {
+            m_client = new HttpClient();
+            m_clientName = clientName;
+            m_token = token;
+            m_server = server;
+            m_lstMemberIds = new List<long>();
+
+            var rnd = new Random();
+            m_lstMemberIds = memberIds.OrderBy(item => rnd.Next()).ToList();
+            m_exportFileName = string.Format("{0}-lst.txt", m_server.Replace('.', '_'));
+            m_logFileName = string.Format("{0}-lst.log", m_server.Replace('.', '_'));
+            RemoveDuplicateId();
+        }
+
+        HttpRequestMessage CreateHttpRequestMessage(string destUrl, HttpContent content) {
+            var msg = new HttpRequestMessage();
+            msg.Method = HttpMethod.Post;
+            msg.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+            msg.Headers.UserAgent.ParseAdd("Mozilla/5.0 (Android; U; zh-CN) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/21.0 rqxbjmdizgzp");
+            msg.Headers.ExpectContinue = false;
+            msg.Headers.Referrer = new Uri("app:/AppMain.swf/[[DYNAMIC]]/1");
+            msg.Headers.Add("x-flash-version", "21,0,0,174");
+            msg.RequestUri = new Uri(destUrl);
+            msg.Content = content;
+            return msg;
+        }
+
+        public async Task StartCacheMembersAsync() {
+            double count = m_lstMemberIds.Count;
+            double current = 0;
+            foreach (var memberId in m_lstMemberIds) {
+                var memberInfo = await GetMemberInfo(memberId);
+                if (memberInfo != null)
+                    Log(string.Format("Current Member Id: {0} acquired. {1:f2}%", memberId, (current / count * 100.0)));
+                else
+                    Log("Current Member Id: " + memberId + " is null.");
+                current++;
+            }
+            Log("RUA.");
+        }
+
+        private async Task<string> GetMemberInfo(long memberId) {
+            try {
+
+                var postContent = new Dictionary<string, string> {
+                        {"api_token", m_token},
+                        {"api_member_id",memberId.ToString()},
+                        {"api_verno","1"},
+                    };
+
+                var httpReqMsg = CreateHttpRequestMessage(string.Format(@"http://{0}/kcsapi/api_req_member/get_practice_enemyinfo", m_server), new FormUrlEncodedContent(postContent));
+
+                var respones = await m_client.SendAsync(httpReqMsg);
+                var rawResult = await respones.Content.ReadAsStringAsync();
+                var rawJson = rawResult.Substring(7);
+                dynamic json = JValue.Parse(rawJson);
+                if ((int)json.api_result != 1) {
+                    string msg = json.api_result_msg;
+                    Log(string.Format("{0}: Error on acquiring member data, code: {1}.", memberId, json.api_result));
+                    if ((int)json.api_result == 201) {
+                        Environment.Exit(0);
+                    }
+                    return null;
+                }
+
+                using (StreamWriter sw = new StreamWriter(new FileStream(m_exportFileName, FileMode.Append), Encoding.UTF8)) {
+                    sw.WriteLine(rawJson);
+                }
+
+                return rawJson;
+            } catch (Exception e) {
+                Log("Exception on GetMember: " + memberId);
+                Log(e.Message);
+                return null;
+            }
+        }
+
+        private void RemoveDuplicateId() {
+            var filesPath = Directory.GetFiles(Directory.GetCurrentDirectory());
+            int prevCount = m_lstMemberIds.Count;
+            try {
+                foreach (var filePath in filesPath) {
+                    string file = Path.GetFileName(filePath);
+                    if (file.Equals(m_exportFileName)) {
+                        using (StreamReader sr = new StreamReader(new FileStream(file, FileMode.Open, FileAccess.Read), Encoding.UTF8)) {
+                            string line;
+                            while ((line = sr.ReadLine()) != null) {
+                                dynamic json = JValue.Parse(line);
+                                long id = (long)json.api_data.api_member_id;
+                                if (!m_lstMemberIds.Remove(id)) {
+                                    Log("Remove ID error: " + id);
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Log("RemoveDuplicateId Exception: " + e.Message);
+                throw;
+            }
+            Log("Total removed duplicated IDs: " + (prevCount - m_lstMemberIds.Count));
+        }
+
+        public void Log(string log) {
+            string s = log + " - by " + m_clientName;
+            Console.WriteLine(s);
+            using (StreamWriter sw = new StreamWriter(new FileStream(m_logFileName, FileMode.Append, FileAccess.Write, FileShare.ReadWrite), Encoding.UTF8)) {
+                sw.WriteLine(string.Format("[{0}] {1}", DateTime.UtcNow.ToString(), s));
+            }
+            
+        }
+    }
+}
